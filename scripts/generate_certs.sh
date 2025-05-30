ed59b3aa79d60504884a09cf1b8e05b40505a104
SanderHuijbers <sander.huijbers@kpn.com>
2025-05-30 11:33:54 +0200

    Add self-signed SSL certificate generation and update Nginx configuration

---
+++ scripts/generate_certs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CERT_DIR="../reverse-proxy/certs"
+# Controleer of de map 'certs' al bestaat en er bestanden in zitten
+if [ -d "$CERT_DIR" ] && [ "$(ls -A $CERT_DIR)" ]; then
+    echo "Certificates already exist in $CERT_DIR. Skipping generation."
+else
+    echo "Generating self-signed certificates in $CERT_DIR..."
+    mkdir -p $CERT_DIR
+    openssl req -x509 -nodes -days 365 \
+        -newkey rsa:2048 -keyout $CERT_DIR/nginx-selfsigned.key \
+        -out $CERT_DIR/nginx-selfsigned.crt \
+        -subj "/CN=localhost"
+    echo "Self-signed certificates generated."
+fi
